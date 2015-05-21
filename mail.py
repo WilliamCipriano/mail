414f03a7c05759a9acc8c40b988ec9433e9ccca9
WilliamCipriano <Github@wfc.help>
2015-05-21 10:56:29 -0400

    Added mail.py

---
+++ mail.py
@@ -0,0 +1,24 @@
+import smtplib
+
+#Can't be named 'email.py'
+#This is really simple, I just have it here as I tend to reuse this code
+
+#Creds
+from_address = '?'
+username = '?'
+password = '?'
+server = 'smtp.gmail.com:587'
+
+def email(recipient, message, subject):
+    global username
+    global password
+    global from_address
+    global server
+    message = 'Subject: %s\n\n%s' % (subject, message)
+    server = smtplib.SMTP(server)
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(from_address, recipient, message)
+    server.quit()
+
+email('alldentex@gmail.com', 'test', 'test')
